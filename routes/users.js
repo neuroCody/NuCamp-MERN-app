60063703d153f08b2af06d10c4db38ce21250b11
Cody Sullivan <76827473+neuroCody@users.noreply.github.com>
2021-05-27 22:29:13 -0600

    Mongoose Population

--- routes/users.js
+++ routes/users.js
@@ -22,11 +22,25 @@ router.post('/signup', (req, res) => {
               res.setHeader('Content-Type', 'application/json')
               res.json({err: err})
           } else {
+            if (req.body.firstname) {
+              user.firstname = req.body.firstname
+            }
+            if (req.body.lastname) {
+              user.lastname = req.body.lastname
+            }
+            user.save(err => {
+              if (err) {
+                res.statuscode = 500
+                res.setHeader('Content-Type', 'application/json')
+                res.json({err: err})
+                return
+              }
               passport.authenticate('local')(req, res, () => {
                   res.statusCode = 200
                   res.setHeader('Content-Type', 'application/json')
                   res.json({success: true, status: 'Registration Successful!'})
               })
+            })
           }
       }
   )
