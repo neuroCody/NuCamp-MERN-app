4696bff03891ae48b99ec272308c002b4466374d
Cody Sullivan <76827473+neuroCody@users.noreply.github.com>
2021-06-05 18:27:00 -0600

    Workshop Week 4

--- routes/favoriteRouter.js
+++ routes/favoriteRouter.js
@@ -119,9 +119,7 @@ favoriteRouter.route('/:campsiteId')
             if (index >= 0) {
                 favorite.campsites.splice(index, 1);
             }
-            /* can alternatively use filter as below, instead of using indexOf/splice, 
-               but make sure to use loose inequality OR do fav.toString() before comparing: */
-            //favorite.campsites = favorite.campsites.filter(fav => fav.toString() !== req.params.campsiteId);
+
             favorite.save()
             .then(favorite => {
                 console.log('Favorite Campsite Deleted!', favorite);
